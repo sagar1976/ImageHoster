9a5dce0de6e24b4095157fca5c2a51c50075311f
sagar1976 <narvekar.sagar@gmail.com>
2019-06-14 10:39:08 +0530

    Add fix for retrieving images with same title

--- src/main/java/ImageHoster/repository/ImageRepository.java
+++ src/main/java/ImageHoster/repository/ImageRepository.java
@@ -47,6 +47,20 @@ public class ImageRepository {
     }
 
     //The method creates an instance of EntityManager
+    //Executes JPQL query to fetch the image from the database with corresponding ImageId
+    //Returns the image in case the image is found in the database
+    //Returns null if no image is found in the database
+    public Image getImageById(Integer imageId) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Image image = em.find(Image.class, imageId);
+            return image;
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    //The method creates an instance of EntityManager
     //Executes JPQL query to fetch the image from the database with corresponding title
     //Returns the image in case the image is found in the database
     //Returns null if no image is found in the database
